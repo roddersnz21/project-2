75611e4f11ee76f53ac371343b7bcbe1b1df593c
roddersnz21 <roddersnz@gmail.com>
2023-01-30 12:12:24 +0000

    Create calculator class to store info for numbers and operations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// Set calculator class to store element references for updating the display 
+class Calculator {
+    constructor(previousOperandandTextElement, currentOperandandTextElement) {
+        this.previousOperandandTextElement = previousOperandandTextElement;
+        this.currentOperandandTextElement = currentOperandandTextElement;
+    }
+}
+
+
+
+// Select all buttons and elements with data attributes
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
