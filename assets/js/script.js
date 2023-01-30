fa36a1c8dd1ec6f291098b9f60fa0837cd795163
roddersnz21 <roddersnz@gmail.com>
2023-01-30 17:45:37 +0000

    Create if statement inside updateDisplay function and concatenate previous operand and operation to display operands

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,7 +74,9 @@ class Calculator {
     updateDisplay() {
         // Set text content of current and previous operand elements
         this.currentOperandTextElement.innerText = this.currentOperand;
-        this.previousOperandTextElement.innerText = this.previousOperand;
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`
+        }
     }
 }
 
